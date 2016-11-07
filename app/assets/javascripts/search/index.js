876b06e60c9537c425812af8a36784c1156edd11
Omar Johnson <admin@omarvelous.com>
2016-11-07 07:37:23 -0500

    some done (#42)
    * some done
    
    * tabs done

--- app/assets/javascripts/search/index.js
+++ app/assets/javascripts/search/index.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
-  var map = $('#search_index_page .map_and_sizing_guide_preview .fixates_on_scrolldown');
-  map.stick_in_parent();
+  if ($(window).width() > 1314){
+    var map = $('#search_index_page .map_and_sizing_guide_preview .fixates_on_scrolldown');
+    map.find('.map iframe').css({ "height": $(window).height() })
+    map.stick_in_parent();
+  }
 })
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 # :nodoc:
 class SearchController < ApplicationController
+  # The search results page should return 3 sizes per location, the cheapest priced unit in the small, medium and large categories.
   def index
   end
 end
