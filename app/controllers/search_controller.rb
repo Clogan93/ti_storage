203004e84b3ebc8d852f86706fc48cd0d7bce759
Omar Johnson <admin@omarvelous.com>
2016-11-07 08:14:35 -0500

    closes #48 (#49)

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 # :nodoc:
 class SearchController < ApplicationController
-  # The search results page should return 3 sizes per location, the cheapest priced unit in the small, medium and large categories.
+  # The search results page should return 3 sizes per location,
+  # the cheapest priced unit in the small, medium and large categories.
   def index
   end
 end
