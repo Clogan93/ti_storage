7b8c74a8b8e4fce97d69cd37dba0248d5caa4c35
lakesare <lakesare@gmail.com>
2016-11-26 13:37:07 +0500

    fixed error with every page code executing on every page

--- app/assets/javascripts/search/index.js.jsx
+++ app/assets/javascripts/search/index.js.jsx
@@ -1,9 +1,10 @@
 $(document).on('turbolinks:load', () => {
   const page = $('#search_index_page');
+  if (page.length == 0){ return }
 
   const mobileMapEl = page.find('section.mobile_map');
   const mapEl = page.find('section.map');
-  // making map sticky on big screens
+
   if ($(window).width() > 1314){
     mapEl.css({ "height": $(window).height() })
     mapEl.stick_in_parent();
