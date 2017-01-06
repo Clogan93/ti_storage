5e24dc9df8b7bd055deccec12aa46e2a7f4e1fa3
lakesare <lakesare@gmail.com>
2017-01-06 07:52:08 +0500

    create sitemap

---
+++ config/sitemap.rb
@@ -0,0 +1,35 @@
+# use rake sitemap:refresh after you update it.
+# it will recreate the /public/sitemap.xml.gz file.
+# it's okay it's gzipped, most search englines read it just fine.
+SitemapGenerator::Sitemap.default_host = "http://tistorage.com"
+SitemapGenerator::Sitemap.create do
+  add '/checkout', changefreq: 'monthly', priority: 0.3
+  add '/search', changefreq: 'monthly', priority: 0.3
+
+  # static pages
+  add '/', changefreq: 'daily', priority: 1
+  add '/storage-solutions', changefreq: 'weekly', priority: 0.7
+  add '/faq', changefreq: 'weekly', priority: 0.7
+  add '/contact', changefreq: 'monthly', priority: 0.3
+
+  # storages
+  add '/locations', changefreq: 'weekly', priority: 0.7
+
+  add '/brooklyn', changefreq: 'weekly', priority: 0.7
+  add '/queens', changefreq: 'weekly', priority: 0.7
+  add '/new-jersey', changefreq: 'weekly', priority: 0.7
+
+  add '/brooklyn/red-hook', changefreq: 'daily', priority: 0.9
+  add '/queens/ozone-park', changefreq: 'daily', priority: 0.9
+  add '/queens/jamaica', changefreq: 'daily', priority: 0.9
+  add '/new-jersey/paterson', changefreq: 'daily', priority: 0.9
+  add '/new-jersey/woodbridge', changefreq: 'daily', priority: 0.9
+
+  # storage solutions
+  add '/art-storage', changefreq: 'weekly', priority: 0.7
+  add '/household-storage', changefreq: 'weekly', priority: 0.7
+  add '/wine-storage', changefreq: 'weekly', priority: 0.7
+  add '/boat-storage', changefreq: 'weekly', priority: 0.7
+  add '/car-storage', changefreq: 'weekly', priority: 0.7
+  add '/commercial-storage', changefreq: 'weekly', priority: 0.7
+end
