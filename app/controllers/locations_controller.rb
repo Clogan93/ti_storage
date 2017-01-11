f4cc451adc277f0d7c8e754e08e465a55b7cd3ad
Omar Johnson <admin@omarvelous.com>
2017-01-10 21:31:36 -0500

    Revert "January 10" (#253)

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
-# rubocop:disable all
-# TODO don't disable all, just can't find how to disable [or VS ||] thing
 # locations == storages
 class LocationsController < ApplicationController
   def index
-    @category = find_category or return(redirect_to('/404'))
+    @category = find_category || return
     @storages = @all_storages.where(category: @category)
   end
 
@@ -12,18 +10,26 @@ class LocationsController < ApplicationController
   end
 
   def show
-    @category = find_category or return(redirect_to('/404'))
-    @storage = find_storage or return(redirect_to('/404'))
+    @category = find_category || return
+    @storage = find_storage || return
     @storage_units = @storage.storage_units.available.order(rent_rate: :asc)
   end
 
   private
 
   def find_category
-    Category.find_by(slug: params[:category_slug])
+    if category = Category.find_by(slug: params[:category_slug])
+      category
+    else
+      redirect_to '/404' && return
+    end
   end
 
   def find_storage
-    @all_storages.find_by(slug: params[:storage_slug])
+    if storage = @all_storages.find_by(slug: params[:storage_slug])
+      storage
+    else
+      redirect_to '/404' && return
+    end
   end
 end
