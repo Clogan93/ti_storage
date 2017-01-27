02c3d3043a89977d3bf29d955207c2bdfaff3b18
Omar Johnson <admin@omarvelous.com>
2017-01-26 22:04:31 -0500

    Add admin/carts (#302)

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,7 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
   namespace :admin do
-    resources :storages do
-      collection do
-        post 'sync'
-      end
-
-      resources :storage_units do
-        collection do
-          post 'sync'
-        end
-      end
-    end
+    resources :carts
   end
 
   resources :subscriptions, only: [:create]
